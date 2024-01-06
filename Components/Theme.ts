b9a400f4aa745bde291e76b0a5acbbae83a4543e
TheCodeFather0 <raminmammadov056@gmail.com>
2024-01-06 13:19:53 +0400

    theme added

--- Components/Theme.ts
+++ Components/Theme.ts
@@ -1,11 +1,25 @@
 import { createTheme } from "@mui/material";
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: "rgba(96, 111, 132, 1)",
+    },
+  },
   components: {
     MuiButton: {
       styleOverrides: {
         root: {
           borderRadius: 0,
+        },
+      },
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          textDecoration: "none",
+          display: "flex",
+          alignItems: "center",
         },
       },
     },
